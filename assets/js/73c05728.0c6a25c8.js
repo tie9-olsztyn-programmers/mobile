73fec0b085298acb4409425b71665ff5577b8516
jarek <js@>
2025-04-06 22:01:08 +0200

    Deploy website - based on ae35077ae8f87e83d8af0654c388772ee94aaed9

---
+++ assets/js/73c05728.0c6a25c8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmobile=self.webpackChunkmobile||[]).push([[602],{3961:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/mobile/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
